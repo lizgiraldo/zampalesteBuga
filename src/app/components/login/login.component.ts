4a10bfb320988c8c8b232b2f7589669ec0ed0627
Lizeth Giraldo <lizgiraldo15@gmail.com>
2023-09-14 18:51:02 -0500

    Validaciones login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginUsuario: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder, 
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) { 
+    this.loginUsuario = this.fb.group({
+      email:['', Validators.required],
+      password:['', Validators.required],
+    })
   }
+
+  ngOnInit(): void {}
+
+  login(){
+    const email = this.loginUsuario.value.email;
+    const password = this.loginUsuario.value.password;
+
+    this.afAuth.signInWithEmailAndPassword(email, password).then((user) => {
+      console.log(user);
+      this.router.navigate(['/dashboard'])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+
 
 }
--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+//import Swal from 'sweetalert2/dist/sweetalert2.js';
+//import 'sweetalert2/src/sweetalert2.scss'
+
+
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -10,9 +15,12 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 export class RegistrarUsuarioComponent implements OnInit {
 registrarUsuario:FormGroup;
+loading: boolean = false;
 
-  constructor(private fb: FormBuilder, 
-    private afAuth: AngularFireAuth
+  constructor(
+    private fb: FormBuilder, 
+    private afAuth: AngularFireAuth,
+    private router: Router
     ) {
     
     this.registrarUsuario = this.fb.group({
@@ -29,12 +37,28 @@ registrarUsuario:FormGroup;
     const email = this.registrarUsuario.value.email;
     const password = this.registrarUsuario.value.password;
     const repetirPassword = this.registrarUsuario.value.repetirPassword;
-    
+
+    if(password !== repetirPassword){
+      alert('Las constraeñas no son iguales');
+      return
+    }
+    this.loading = true;
     this.afAuth.createUserWithEmailAndPassword(email, password).then((user)=>{
-      console.log(user)
+      this.loading = false;
+      alert('El usuario ha sido registrado con exito')
+      this.router.navigate(['/login'])
+      
     }).catch((error)=> {
+
       console.log(error);
+      this.loading = false;
       alert(this.firebaseError(error.code))
+      // Swal.fire({
+      //   icon: 'error',
+      //   title: 'Oops...',
+      //   text: 'Something went wrong!',
+      //   footer: '<a href="">Why do I have this issue?</a>'
+      // })
     })
   }
 
